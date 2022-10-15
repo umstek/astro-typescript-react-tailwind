f182b3901cc9defd432e4a1c19ad8a60c88bd1c4
Wickramaranga Abeygunawardhana <umstek@live.com>
2022-10-15 20:54:34 +0530

    fix(rss): :bug: fix rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,10 +2,17 @@ import rss from '@astrojs/rss';
 
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
+const postImportResult = import.meta.glob('./blog/**/*.{md,mdx}', { eager: true });
+const posts = Object.values(postImportResult);
+
 export const get = () =>
   rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: import.meta.glob('./blog/**/*.{md,mdx}'),
+    items: posts.map((post: any) => ({
+      link: post.url,
+      title: post.frontmatter.title,
+      pubDate: post.frontmatter.publishedAt,
+    })),
   });
